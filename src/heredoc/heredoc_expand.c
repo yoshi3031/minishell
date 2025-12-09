1c13b653e0c57ca9d32dd77df2a138db6265a0d4
¨o4kira¨ <akiramax563@gmail.com>
2025-12-09 05:07:57 +0000

    norm_heredoc

---
+++ src/heredoc/heredoc_expand.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_expand.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayamamot <ayamamot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 03:02:10 by ayamamot          #+#    #+#             */
+/*   Updated: 2025/12/09 03:31:31 by ayamamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*get_expand_value(char *str, int *i, char **env, int status)
+{
+	int		len;
+	char	*key;
+	char	*val;
+
+	if (str[*i + 1] == '?')
+	{
+		*i += 2;
+		return (ft_itoa(status));
+	}
+	*i += 1;
+	len = 0;
+	while (str[*i + len] && (ft_isalnum(str[*i + len]) || str[*i + len] == '_'))
+		len++;
+	key = ft_substr(str, *i, len);
+	val = get_env_value(env, key);
+	free(key);
+	*i += len;
+	return (val);
+}
+
+static void	append_value(char *dst, size_t *current_len, char *val)
+{
+	size_t	val_len;
+
+	if (!val)
+		return ;
+	val_len = ft_strlen(val);
+	if (dst)
+		ft_strlcpy(dst + *current_len, val, val_len + 1);
+	*current_len += val_len;
+}
+
+static size_t	perform_expansion(char *line, char **env, int status, char *dst)
+{
+	size_t	len;
+	int		i;
+	char	*val;
+
+	len = 0;
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '$' && (line[i + 1] == '?' || ft_isalpha(line[i + 1])
+				|| line[i + 1] == '_'))
+		{
+			val = get_expand_value(line, &i, env, status);
+			append_value(dst, &len, val);
+			free(val);
+		}
+		else
+		{
+			if (dst)
+				dst[len] = line[i];
+			len++;
+			i++;
+		}
+	}
+	return (len);
+}
+
+char	*expand_heredoc_line(char *line, char **env, int status)
+{
+	char	*new_line;
+	size_t	len;
+
+	len = perform_expansion(line, env, status, NULL);
+	new_line = (char *)malloc(sizeof(char) * (len + 1));
+	if (!new_line)
+		return (NULL);
+	perform_expansion(line, env, status, new_line);
+	new_line[len] = '\0';
+	return (new_line);
+}
---
+++ src/heredoc/heredoc_utils.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayamamot <ayamamot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 04:57:48 by ayamamot          #+#    #+#             */
+/*   Updated: 2025/12/09 05:02:44 by ayamamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	abort_heredoc(char *line, int pipe_fd[2])
+{
+	free(line);
+	close(pipe_fd[1]);
+	close(pipe_fd[0]);
+	rl_event_hook = NULL;
+	init_signals();
+	return (-1);
+}
+
+void	setup_heredoc_signals(void)
+{
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = heredoc_sigint_handler;
+	sigaction(SIGINT, &sa, NULL);
+}
+
+int	heredoc_signal_check(void)
+{
+	if (g_signal)
+		rl_done = 1;
+	return (0);
+}
+
+void	check_heredoc_eof(char *line)
+{
+	if (!line)
+		ft_putstr_fd("minishell: warning: "
+			"here-document delimited by end-of-file\n", 2);
+	else
+		free(line);
+}
